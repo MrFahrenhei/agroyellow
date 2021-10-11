2039d2282e92b9de7a321f359d61ba89789664fe
Vitor Dietrich <vitordietrich@outlook.com>
2021-10-11 20:57:36 -0300

    master

--- conexao.sql
+++ conexao.sql
@@ -17,15 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `conexao`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `empresa`
---
 
 CREATE TABLE `empresa` (
   `ID` int(11) NOT NULL,
