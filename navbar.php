20b478e1df22b22b7e4a78e8780f526b0136a23d
Vinicius Valle Beraldo <viniciusvalleb@hotmail.com>
2021-10-15 19:56:52 -0300

    alterações no navbar

--- navbar.php
+++ navbar.php
@@ -2,15 +2,15 @@
 <link rel="stylesheet" href="styles/navbar.css">
 
 <ul>
-  <li><img src="src/logo.png"></li>
   <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">Geral</a></li>
+   <?php  if ($_SESSION['nome'] == true){
+      echo '<li  style="background-color: blue; !important"><a href="perfil.php">'.$_SESSION['nome'].'</a></li>';
+   }?>
   <li><a href="#contact">Contato</a></li>
   <li><a href="#about">Sobre</a></li>
 
   <?php
     if ($_SESSION['nome'] == true) {
-        echo '<li  style="background-color: blue; !important"><a href="perfil.php"> Seu Perfil : '.$_SESSION['nome'].'</a></li>';
         echo '<li style="float:right; background-color: red; !important"><a href="logout.php">Sair</a></li>
         ';
     }
