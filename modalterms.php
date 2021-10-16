82d61b47fed9f418795d7bcd42f637121588e6f1
Daniel_Zury15 <90483966+Daniel-Zury15@users.noreply.github.com>
2021-10-15 21:30:43 -0300

    formatar

--- modalterms.php
+++ modalterms.php
@@ -2,37 +2,37 @@
 
 <div>
 
-<input type="checkbox" id="scales" name="scales" value="on" >
+	<input type="checkbox" id="scales" name="scales" value="on">
 
 </div>
-<p>Por estar criando uma conta, você concorda com os nossos <a  onclick="document.getElementById('modalterms').style.display='block'"   style="width:auto;"> Termos & Privacidades</a> </p>
+<p>Por estar criando uma conta, você concorda com os nossos <a onclick="document.getElementById('modalterms').style.display='block'" style="width:auto;"> Termos & Privacidades</a> </p>
 
 
 <div id="modalterms" class="modalterms">
-        <div class="modal-contents">
-            <span class="closer">&times;</span>
-            <?php  include "Terms.php" ;?> 
-            <button onclick="document.getElementById('modalterms').style.display='none'" >Li e Concordo com os Termos</button>
-        </div>
-        <br>
+	<div class="modal-contents">
+		<span class="closer">&times;</span>
+		<?php include "Terms.php"; ?>
+		<button onclick="document.getElementById('modalterms').style.display='none'">Li e Concordo com os Termos</button>
+	</div>
+	<br>
 </div>
 <script>
-  document.getElementById('modalterms').onclick = function() {myFunction()};
-  var scales = document.getElementById('scales');
+	document.getElementById('modalterms').onclick = function() {
+		myFunction()
+	};
+	var scales = document.getElementById('scales');
 
-function myFunction() {
-	console.log("teste");
-	console.log(scales.checked);
-	scales.checked = true;
-}
+	function myFunction() {
+		console.log("teste");
+		console.log(scales.checked);
+		scales.checked = true;
+	}
 
-  window.onclick = function(event) {
-    if (event.target == modalterms) {
-      modalterms.style.display = "none";
+	window.onclick = function(event) {
+		if (event.target == modalterms) {
+			modalterms.style.display = "none";
 
-    }
-  
-  }
+		}
+
+	}
 </script>
-
-
