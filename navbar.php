b0d20ac4ab2e8ee635c4cb5bb1adead5fc676db0
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2021-10-17 00:05:55 -0300

    Nav bar ta um aço

--- navbar.php
+++ navbar.php
@@ -3,24 +3,23 @@
 
 <div id="navPrincipal" class="menuContainer fixed">
   <div id="menuMaior">
-      <a href="">Logo</a>
-      <div id="pesqusia">Pesquisa</div>
       <div class="canto">
+          <a href="">Logo</a>
+          <input type="text" placeholder="Procurar..">
           <a href="#home">Compras</a>
           <a href="#contact">Vender</a>
           <?php
             if ($_SESSION['nome'] == true) {
-            echo'<a style=" !important"  href="perfil.php">'.$_SESSION['nome'].'</a>';
-            echo '<a style="float:right; border: none;!important" href="logout.php">Sair</a>';}
+            echo'<a href="perfil.php">'.$_SESSION['nome'].'</a>';
+            echo'<a style="background-color:red; border-radius:5px; !important" href="logout.php">Sair</a>';}
           ?>
       </div>
   </div>
   <div id="menuMenor">
-    <ul>
+      <ul>
         <li><a href="#home">Home</a></li>
-        <li><a href="#contact">test</a></li>
         <li><a href="#contact">Contato</a></li>
         <li><a href="#about">Sobre</a></li>
-        </ul>
+      </ul>
   </div>
 </div>
