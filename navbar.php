b985d05f436aeb0177bfb79c0b350e3bf4616353
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2021-10-16 22:32:29 -0300

    css da home

--- navbar.php
+++ navbar.php
@@ -1,26 +1,26 @@
 
 <link rel="stylesheet" href="styles/navbar.css">
-<ul style="padding:5% 0% 2% 45%; !important">
-  <?php  if ($_SESSION['nome'] == true){
-      echo '<li style="background-color: blue;border-radius: 10px; !important"><a href="perfil.php">'.$_SESSION['nome'].'</a></li>';
-   }?>
-</ul>
-<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#contact">test</a></li>
-  <li><a href="#contact">Contato</a></li>
-  <li><a href="#about">Sobre</a></li>
 
-  <?php
-    if ($_SESSION['nome'] == true) {
-        echo '<li style="float:right; border: none;!important"><a href="logout.php">Sair</a></li>
-        ';
-    }
-    ?>
-    <?php
-          if (isset($_SESSION['user'])) {
-            echo ' 
-            <li><a>Painel Usuário</a></li>
-            ' ;   
-          }     ?> 
-</ul>
+<div id="navPrincipal" class="menuContainer fixed">
+  <div id="menuMaior">
+      <a href="">Logo</a>
+      <div id="pesqusia">Pesquisa</div>
+      <div class="canto">
+          <a href="#home">Compras</a>
+          <a href="#contact">Vender</a>
+          <?php
+            if ($_SESSION['nome'] == true) {
+            echo'<a style=" !important"  href="perfil.php">'.$_SESSION['nome'].'</a>';
+            echo '<a style="float:right; border: none;!important" href="logout.php">Sair</a>';}
+          ?>
+      </div>
+  </div>
+  <div id="menuMenor">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#contact">test</a></li>
+        <li><a href="#contact">Contato</a></li>
+        <li><a href="#about">Sobre</a></li>
+        </ul>
+  </div>
+</div>
