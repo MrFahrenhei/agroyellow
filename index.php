ab8dce8f81543d9e1754826034e8a78b622a7f30
Daniel_Zury15 <90483966+Daniel-Zury15@users.noreply.github.com>
2021-10-18 04:18:20 -0300

    for

--- index.php
+++ index.php
@@ -7,29 +7,31 @@ $db = mysqli_select_db($cx, "conexao");
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <link rel="shortcut icon" href="src/icon.ico"/> 
-    <link rel="stylesheet" href="styles/reset.css">
-    <link rel="stylesheet" href="styles/css.css">
-    <script type="text/javascript" src="index.js"></script>
-    <title>Login </title>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="src/icon.ico" />
+	<link rel="stylesheet" href="styles/reset.css">
+	<link rel="stylesheet" href="styles/css.css">
+	<script type="text/javascript" src="index.js"></script>
+	<title>Login </title>
 </head>
 
 <body>
-   <div id="corpao">
-    <h2> O que você gostaria de fazer?</h2> 
-    <br>
-    <a href="empresa.php"><button style="width: 50%;" !important>Vender</button></a>
-    <br>
-    <br>
-    <a href="user.php"><button style="width: 50%;" !important>comprar</button></a>
-    <br><br><br><br>
-    <div class="container signin">
-        <?php include "modal.php" ;?>
-    </div>
-    <br>
-</div>
-   
+	<div id="corpao">
+		<h2> O que você gostaria de fazer?</h2>
+		<br>
+		<a href="empresa.php"><button style="width: 50%;" !important>Vender</button></a>
+		<br>
+		<br>
+		<a href="user.php"><button style="width: 50%;" !important>comprar</button></a>
+		<br><br><br><br>
+		<div class="container signin">
+			<?php include "modal.php"; ?>
+		</div>
+		<br>
+	</div>
+
 </body>
+
 </html>
