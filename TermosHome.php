835e7b855e218e6e1ca6675d28adaf64dd90a89d
VitorDietrich-coder <vitorgames078@gmail.com>
2021-11-08 20:46:46 -0300

    commit

--- TermosHome.php
+++ TermosHome.php
@@ -26,10 +26,11 @@ if (!isset($_SESSION['nome'])) {
 
 ?>
 <div class="modal-contentsHome">
+  <br>
     <a class="termos"> Termos De Uso  </a>
     <div class="qualquercoisa1">
 
-      <p> <center>  CONSENTIMENTO PARA TRATAMENTO DE DADOS PESSOAIS LEI GERAL DE PROTEÇÃO DE DADOS PESSOAIS – LGPD  </center><br> <br>
+     <a class="text"> <center>  CONSENTIMENTO PARA TRATAMENTO DE DADOS PESSOAIS LEI GERAL DE PROTEÇÃO DE DADOS PESSOAIS – LGPD  </center><br> <br>
           
     &emsp;  Este documento visa registrar a manifestação livre, informada e inequívoca pela qual o Titular concorda com o tratamento de seus dados pessoais para finalidade específica, em conformidade com a Lei n.º 13.709 – Lei Geral de Proteção de Dados Pessoais (LGPD). 
     <br>
@@ -56,11 +57,11 @@ if (!isset($_SESSION['nome'])) {
       3.	DAS DISPOSIÇÕES FINAIS 
       <br>
       &emsp;Ao declarar que concorda com o presente termo, o Titular consente com a coleta, armazenamento, tratamento, processamento e uso das informações enviadas e/ou transmitidas nos rigores estabelecidos neste Termo de Consentimento.
-   </p>
-
+</a>
+   </a>
 	</div>
  <a type="submit"  id="submit" href="home.php" >voltar</a>
-</div>
+
 
 
 
