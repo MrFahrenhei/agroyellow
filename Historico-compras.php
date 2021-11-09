344160483b753b4817d81b7b851b86b65bf8f70c
VitorDietrich-coder <vitorgames078@gmail.com>
2021-11-08 21:31:58 -0300

    commit

---
+++ Historico-compras.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+error_reporting(0);
+
+if (!isset($_SESSION['nome'])) {
+    header("Location: index.php");
+    exit;
+  }
+  
+  else{
+    if (!isset($_SESSION['senha'])) {
+      header("Location: index.php");
+      exit;
+    }
+  }
+
+?>
+
+<link rel="stylesheet" href="styles/payments.css">
+
+<?php 
+    include "navbar.php";
+
+?>
+    
