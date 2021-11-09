850f2adb665664560b721e4de0d8eb0251a403bc
VVBeraldo <vvberaldo@minha.fag.edu.br>
2021-11-08 21:41:20 -0300

    o perfil (não finalizado)

--- perfil.php
+++ perfil.php
@@ -20,24 +20,77 @@ if (!isset($_SESSION['nome'])) {
         <meta charset="UTF-8">
         <link rel="stylesheet" href="styles/test.css">
         <link rel="stylesheet" href="styles/css.css">
+        <link rel="stylesheet" href="styles/perfil.css">
         <link rel="shortcut icon" href="src/icon.ico"/> 
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script type="text/javascript" href="index.js" src="index.js"></script>
 
-        <title>HOME</title>
+        <title>Perfil</title>
     </head>
     <body>
+    <div class="container">
       <header>
           <?php include "navbar.php"; ?>
+          <i class="fa fa-bars" aria-hidden="true"></i>
       </header>
-    <form>
-        <h1>
-        <?php
-            if ($_SESSION['nome'] == true) {
-            echo'<a href="perfil.php" style="color:red; padding:10% 0% 5% 50%;">'.$_SESSION['nome'].'</a>';
-            }
-          ?>
-        </h1>
-    </form>
+    <main>
+      <div class="row">
+
+        <div class="left">
+          <div class="photo-left">
+            <img class="photo" src="https://yt3.ggpht.com/ytc/AKedOLQiqn6FdeCgAdYXD8NcSLnNedggPHLniR8djQaCow=s88-c-k-c0x00ffffff-no-rj" alt="Ainda tem nada aki filhão">
+            <div class="active"></div>
+          </div>
+
+        <h4 class="name"><?php
+         if ($_SESSION['nome'] == true) {
+            echo $_SESSION['nome'];
+            }?>
+        </h4>
+
+        <p class="info">Capeão profissional</p>
+        <p class="info"><?php
+         if ($_SESSION['U_email'] == true) {
+            echo $_SESSION['U_email'];
+            }?></p>
+
+        <div class="stats row">
+            <div class="stat col-xs-4" >
+              <p class="number-stat">12</p>
+              <p class="desc-stat">Seguidores</p>
+            </div>
+            <div class="stat col-xs-4">
+              <p class="number-stat">1</p>
+              <p class="desc-stat">Seguindo</p>
+            </div>
+            <div class="stat col-xs-4">
+              <p class="number-stat">1</p>
+              <p class="desc-stat">Publicações</p>
+            </div>
+        </div>
+
+        <p class="desc">Eu gosto delas grandes, eu gosto delas robustas<br>
+Eu gosto delas grandes, eu gosto delas fofinhas<br>
+Eu gosto delas por perto, com algo a mais<br>
+Elas gostam do meu som, elas acham que eu sou malcheiroso</p>
+        <div class="social">
+          <i class="fa fa-facebook-square" aria-hidden="true"></i>
+          <i class="fa fa-twitter-square" aria-hidden="true"></i>
+          <i class="fa fa-pinterest-square" aria-hidden="true"></i>
+          <i class="fa fa-tumblr-square" aria-hidden="true"></i>
+        </div>
+
+      </div>
+
+      <div class="right col-lg-8">
+        <ul class="nav">
+          <li>Vendas</li>
+          <li>Compras</li>
+          <li>Configurações</li>
+          <li>Sobre</li>
+        </ul>    
+      </div>
+
+    </main>
+    </div>
     </body>
 </html>
