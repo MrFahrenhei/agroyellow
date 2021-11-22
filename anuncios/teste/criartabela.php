491ecfd5c89d9da66d42dcad252a6d125a8332ba
Daniel_Zury15 <90483966+Daniel-Zury15@users.noreply.github.com>
2021-11-22 20:04:48 -0300

    anuncios

--- anuncios/teste/criartabela.php
+++ anuncios/teste/criartabela.php
@@ -2,20 +2,19 @@
 
 include("conexeção1.php");
 
-// sql to create table
 $sql = "CREATE TABLE Compra (
 ID INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Titulo VARCHAR(50) NOT NULL,
 Descriçao VARCHAR(500) NOT NULL,
-Valor REAL(20) NOT NULL,
-Quantidade INT(20) NOT NULL,
-Peso DOUBLE(20) NOT NULL,
-Frete VARCHAR(20) NOT NULL
+Valor VARCHAR(50) NOT NULL,
+Quantidade INT(50) NOT NULL,
+Peso VARCHAR(50) NOT NULL,
+Frete VARCHAR(50) NOT NULL
 
 )";
 
 if ($conn->query($sql) === TRUE) {
-	echo "Table MyGuests created successfully";
+	echo "Table Compra created successfully";
 } else {
 	echo "Error creating table: " . $conn->error;
 }
