6c94d0ad73b50a5d3ebc0146a35667073a0eeba3
VVBeraldo <vvberaldo@minha.fag.edu.br>
2021-11-22 20:18:09 -0300

    geki

--- Footer.php
+++ Footer.php
@@ -2,46 +2,16 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 <div class="footer">
         <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="item">
-                        <h3>Serviços</h3>
-                        <ul>
-                            <li><a href="#">Home</a></li>
+                    <div class="item-um">
+                        <ul style="order: 1" class="coluna-um">
+                            <li><h3>Informações</h3></li>
+                            <li><a href="#">Endereço</a></li>
+                            <li><a href="#">CEP</a></li>
                             <li><a href="#">Sobre</a></li>
-                            <li><a href="#">Contato</a></li>
+                            <li class="copyright">Agroyellow © 2021</li>
+                            <img style="float:right" src="src/LogoAgroYellow.png">
                         </ul>
                     </div>
-                    <div class="item">
-                        <h3>Sobre</h3>
-                        <ul>
-                            <li><a href="#">Configuração</a></li>
-                            <li><a href="#">Termos de uso</a></li>
-                            <li><a href="#">Sobre</a></li>
-                        </ul>
-                    </div>
-                    <div class="item">
-                        <h3>Coisa</h3>
-                        <ul>
-                            <li><a href="#">Histórico</a></li>
-                            <li><a href="#">Chat</a></li>
-                            <li><a href="#">Perfil</a></li>
-                        </ul>
-                    </div>
-                    <div class="social">
-                        <a href="#">
-                            <i class="icon ion-social-facebook"></i>
-                        </a>
-                        <a href="#">
-                            <i class="icon ion-social-twitter"></i>
-                        </a>
-                       <a href="#">
-                            <i class="icon ion-social-instagram"></i>
-                        </a>
-                        <p class="copyright">Agroyellow © 2021</p>
-                    </div>
-                </div>
-            </div>
         </footer>
     </div>
 </div>
